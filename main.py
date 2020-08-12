c422ae5c0dd0259084f2c51857c8ff0ee7d80bb6
Stefan <sjovicevic@protonmail.ch>
2020-08-12 12:26:23 +0200

    project initialization

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+# Tic Tac Toe game will be developed in 4 stages
+
+# 1st stage - drawing a board game
+# 2nd stage - check tic tac toe
+# 3rd stage - tic tac toe draw 
+# 4th stage - finalization and adding details
+
+# idea & developing plan: practicepython.org 
+# author : stefan jovicevic
+# github: github.com/sjovicevic
+# august 2020
+# serbia
+
